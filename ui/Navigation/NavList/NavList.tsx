39d95fb783f1cf096c8bf51b19a21ff985074566
Mateusz Glowania <mglowani@createctrl.com>
2022-08-01 22:01:14 +0200

    Init NavList

---
+++ ui/Navigation/NavList/NavList.tsx
@@ -0,0 +1,6 @@
+import { ReactElement } from 'react';
+
+const NavList = (): ReactElement => {
+
+  return <>Hi</>
+}
---
+++ ui/Navigation/NavList/NavListStyled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const NavListWrapper = styled.div``
+
+const NavList = styled.ul``
+
+const NavListItem = styled.a``
+
+export { NavListWrapper, NavList, NavListItem }
